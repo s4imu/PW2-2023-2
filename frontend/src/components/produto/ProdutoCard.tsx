d15f421f95b751a1382f56eb25c1949cd3022e5f
Symon Barreto <contatosymon@gmail.com>
2024-03-06 22:58:26 -0400

    incrementar e decrementar valores de quantidade e preço total

--- frontend/src/components/produto/ProdutoCard.tsx
+++ frontend/src/components/produto/ProdutoCard.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -14,27 +14,56 @@ interface ProdutoCardProps {
 }
 
 export default function ProdutoCard({ produto }: ProdutoCardProps) {
+  let [quantidade, setQuantidade] = useState(1);
+  let [precoTotal, setPrecoTotal] = useState(1 * produto.preco);
+
+  useEffect(() => {
+    setPrecoTotal(quantidade * produto.preco);
+  }, [quantidade]);
+
+  const increaseQtd = () => {
+    if (quantidade < produto.estoque) setQuantidade((quantidade += 1));
+  };
+
+  const decreaseQtd = () => {
+    if (quantidade > 1) setQuantidade((quantidade -= 1));
+  };
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 445 }}>
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {produto.nome}
-        </Typography>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "start",
+          }}
+        >
+          <Typography gutterBottom variant="h4" component="div">
+            {produto.nome}
+          </Typography>
+          <ButtonGroup
+            size="small"
+            variant="contained"
+            aria-label="Basic button group"
+          >
+            <Button onClick={increaseQtd} variant="contained" size="small">
+              <AddIcon />
+            </Button>
+            <Button onClick={decreaseQtd} variant="contained" size="small">
+              <RemoveIcon />
+            </Button>
+          </ButtonGroup>
+        </div>
+
         <Typography variant="body2" color="text.secondary">
           Preço: {produto.preco} <br />
-          Estoque: {produto.estoque}
+          Estoque: {produto.estoque} <br />
+          Quantidade: {quantidade} <br />
+          Preço Total: {precoTotal}
         </Typography>
       </CardContent>
-      <CardActions>
-        <ButtonGroup variant="contained" aria-label="Basic button group">
-          <Button variant="contained" size="small">
-            <AddIcon />
-          </Button>
-          <Button variant="contained" size="small">
-            <RemoveIcon />
-          </Button>
-        </ButtonGroup>
-      </CardActions>
+      <CardActions></CardActions>
     </Card>
   );
 }
