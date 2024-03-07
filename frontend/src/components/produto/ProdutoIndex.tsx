91e68a0923891c0a4cafe5afcc3fe5626a36df09
Symon Barreto <contatosymon@gmail.com>
2024-03-06 23:18:35 -0400

    refatorando o produto

---
+++ frontend/src/components/produto/ProdutoIndex.tsx
@@ -0,0 +1,21 @@
+import { getAllProdutos } from "@/fakeDb/produtos";
+import styles from "./produto.module.css";
+import Link from "next/link";
+
+function ProdutoIndex() {
+  const produtos = getAllProdutos();
+  return (
+    <div>
+      <h1>Produtos</h1>
+      <ol>
+        {produtos.map((prod) => (
+          <li key={prod.id} className={styles.listaProdutos}>
+            <Link href={`/produto/${prod.id}`}>{prod.nome}</Link>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default ProdutoIndex;
--- frontend/src/pages/produto/index.tsx
+++ frontend/src/pages/produto/index.tsx
@@ -1,13 +1,9 @@
-import { getAllProdutos } from "@/fakeDb/produtos";
-import styles from "./produto.module.css"
-import Link from "next/link";
+import React from "react";
 
-export default function Home() {
-  const produtos = getAllProdutos();
-  return (
-    <div>
-     <h1>Produtos</h1>
-     <ol>{produtos.map(prod => <li key={prod.id} className={styles.listaProdutos}><Link href={`/produto/${prod.id}`}>{prod.nome}</Link></li>)}</ol>
-    </div>
-  );
+import ProdutoIndex from "@/components/produto/ProdutoIndex";
+
+function ProdutoIndexPage() {
+  return <ProdutoIndex />;
 }
+
+export default ProdutoIndexPage;
